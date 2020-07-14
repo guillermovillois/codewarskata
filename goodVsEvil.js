b139dcff01238cc7d0cb5735ee51318bdcd9c6ff
Guille <guillearg@gmail.com>
2020-07-13 22:16:48 -0300

    good vs evil

---
+++ goodVsEvil.js
@@ -0,0 +1,4 @@
+
+
+
+Return "Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins, or "Battle Result: No victor on this battle field" if it ends in a tie.
