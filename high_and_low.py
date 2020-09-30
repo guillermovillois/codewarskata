533fd84d5294562e3c3183605be20c38c835e23e
Guille <guillearg@gmail.com>
2020-09-30 14:46:11 -0300

    low and high

--- high_and_low.py
+++ high_and_low.py
@@ -1,16 +1,6 @@
 def high_and_low(numbers):
     numbers = [int(x) for x in numbers.split(' ')]
-    for i, each in enumerate(numbers):
-        if i == 0:
-            high = each
-            low = each
-        else:
-            if each > high:
-                print(each)
-                high = each
-            elif each < low:
-                low = each
-    print(low, high)
+    print('%x %x' % (max(numbers), min(numbers)))
 
 
 high_and_low("1 2 3 4 5")  # return "5 1"
