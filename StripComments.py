ab9281ca8cd1a71634e33082cd40cb463523cd94
Guille <guillearg@gmail.com>
2020-10-04 00:28:46 -0300

    stripComments update

--- StripComments.py
+++ StripComments.py
@@ -1,20 +1,13 @@
 def solution(string, markers):
-    final_string = string
-    for each in markers:
-        finalstr = final_string.find(
-            '''\n''', final_string.find(each))
-        if finalstr == -1:
-            final_string = final_string.split(each)[0]
-            break
-        start = final_string[:final_string.find(each)]
-        end = final_string[finalstr:]
-        final_string = start + end
-
-    print(str(final_string))
-    # your code here
+    lista = string.split('\n')
+    for i, each in enumerate(lista):
+        for marker in markers:
+            each = each.split(marker)[0]
+        lista[i] = each
+    print('\n'.join(lista))
 
 
-result = solution(
-    "apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
-solution("a #b\nc\nd $e f g", ["#", "$"])
-solution('.\navocados watermelons ?\npears', ['#', '=', ',', '-', "'", '.'])
+# result = solution(
+#     "apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+solution('cherries strawberries cherries ? oranges =\npears avocados\nbananas\n! pears bananas\n.', [
+         '?', '^', '@', '!', '#'])
